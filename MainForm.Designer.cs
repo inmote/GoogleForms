d4f76d6367542700ee6e1fd2c78355b1ad9fe8aa
ruben <ruben@inmote.com>
2025-03-12 10:38:17 +0100

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace GoogleForms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnXLSBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbCsvInputFile = new System.Windows.Forms.TextBox();
+            this.tbXLSTemplate = new System.Windows.Forms.TextBox();
+            this.pbProgress = new System.Windows.Forms.ProgressBar();
+            this.tbXLSOutputFolder = new System.Windows.Forms.TextBox();
+            this.btnOutputBrowse = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(466, 22);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(169, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "&Browse (set CSV input file)";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnXLSBrowse
+            // 
+            this.btnXLSBrowse.Location = new System.Drawing.Point(466, 60);
+            this.btnXLSBrowse.Name = "btnXLSBrowse";
+            this.btnXLSBrowse.Size = new System.Drawing.Size(169, 23);
+            this.btnXLSBrowse.TabIndex = 1;
+            this.btnXLSBrowse.Text = "&Browse (set XLS template file)";
+            this.btnXLSBrowse.UseVisualStyleBackColor = true;
+            this.btnXLSBrowse.Click += new System.EventHandler(this.btnXLSBrowse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "CSV file with responses:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "XLS template";
+            // 
+            // tbCsvInputFile
+            // 
+            this.tbCsvInputFile.Enabled = false;
+            this.tbCsvInputFile.Location = new System.Drawing.Point(12, 25);
+            this.tbCsvInputFile.Name = "tbCsvInputFile";
+            this.tbCsvInputFile.ReadOnly = true;
+            this.tbCsvInputFile.Size = new System.Drawing.Size(448, 20);
+            this.tbCsvInputFile.TabIndex = 4;
+            // 
+            // tbXLSTemplate
+            // 
+            this.tbXLSTemplate.Enabled = false;
+            this.tbXLSTemplate.Location = new System.Drawing.Point(12, 63);
+            this.tbXLSTemplate.Name = "tbXLSTemplate";
+            this.tbXLSTemplate.ReadOnly = true;
+            this.tbXLSTemplate.Size = new System.Drawing.Size(448, 20);
+            this.tbXLSTemplate.TabIndex = 5;
+            // 
+            // pbProgress
+            // 
+            this.pbProgress.Enabled = false;
+            this.pbProgress.Location = new System.Drawing.Point(12, 128);
+            this.pbProgress.Name = "pbProgress";
+            this.pbProgress.Size = new System.Drawing.Size(448, 23);
+            this.pbProgress.TabIndex = 6;
+            // 
+            // tbXLSOutputFolder
+            // 
+            this.tbXLSOutputFolder.Enabled = false;
+            this.tbXLSOutputFolder.Location = new System.Drawing.Point(12, 102);
+            this.tbXLSOutputFolder.Name = "tbXLSOutputFolder";
+            this.tbXLSOutputFolder.ReadOnly = true;
+            this.tbXLSOutputFolder.Size = new System.Drawing.Size(448, 20);
+            this.tbXLSOutputFolder.TabIndex = 7;
+            // 
+            // btnOutputBrowse
+            // 
+            this.btnOutputBrowse.Location = new System.Drawing.Point(466, 99);
+            this.btnOutputBrowse.Name = "btnOutputBrowse";
+            this.btnOutputBrowse.Size = new System.Drawing.Size(169, 23);
+            this.btnOutputBrowse.TabIndex = 8;
+            this.btnOutputBrowse.Text = "&Browse (set XLS output folder)";
+            this.btnOutputBrowse.UseVisualStyleBackColor = true;
+            this.btnOutputBrowse.Click += new System.EventHandler(this.btnOutputBrowse_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "XLS output folder:";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(466, 128);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(169, 23);
+            this.btnGenerate.TabIndex = 10;
+            this.btnGenerate.Text = "Generate output XLS files";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 167);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnOutputBrowse);
+            this.Controls.Add(this.tbXLSOutputFolder);
+            this.Controls.Add(this.pbProgress);
+            this.Controls.Add(this.tbXLSTemplate);
+            this.Controls.Add(this.tbCsvInputFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnXLSBrowse);
+            this.Controls.Add(this.btnBrowse);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Google Forms v1.0.0.0";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnXLSBrowse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbCsvInputFile;
+        private System.Windows.Forms.TextBox tbXLSTemplate;
+        private System.Windows.Forms.ProgressBar pbProgress;
+        private System.Windows.Forms.TextBox tbXLSOutputFolder;
+        private System.Windows.Forms.Button btnOutputBrowse;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGenerate;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,350 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+using Text = DocumentFormat.OpenXml.Spreadsheet.Text;
+
+namespace GoogleForms
+{
+    public partial class MainForm : Form
+    {
+        private String mCSVInputfile;
+        private String mXLSTemplatefile;
+        private String mXLSOutputFolder;
+        private Dictionary<String, String> map = new Dictionary<String, String>
+        {
+            { "\"Zeer mee oneens\"",       "C" },
+            { "\"Mee oneens\"",            "D" },
+            { "\"Gedeeltelijk mee eens\"", "E" },
+            { "\"Mee eens\"",              "F" },
+            { "\"Zeer mee eens\"",         "G" },
+            { "\"Geen antwoord\"",         "H" }
+        };
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Text = "Google Forms v" + Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            ReadSettings();
+            if (File.Exists(mCSVInputfile))
+            {
+                tbCsvInputFile.Text = mCSVInputfile;
+            }
+            else
+            {
+                tbCsvInputFile.Text = mCSVInputfile = "";
+            }
+            if (File.Exists(mXLSTemplatefile))
+            {
+                tbXLSTemplate.Text = mXLSTemplatefile;
+            }
+            else
+            {
+                tbXLSTemplate.Text = mXLSTemplatefile = "";
+            }
+            if (Directory.Exists(mXLSOutputFolder))
+            {
+                tbXLSOutputFolder.Text = mXLSOutputFolder;
+            }
+            else
+            {
+                tbXLSOutputFolder.Text = mXLSOutputFolder = "";
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\",
+                Title = "Select CSV input files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "csv",
+                Filter = "CSV files (*.csv)|*.csv",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                mCSVInputfile = tbCsvInputFile.Text = openFileDialog.FileName;
+                WriteSettings();
+            }
+        }
+        Cell GetSpreadsheetCell(Worksheet worksheet, string columnName, uint rowIndex)
+        {
+            IEnumerable<Row> rows = worksheet.GetFirstChild<SheetData>()?.Elements<Row>().Where(r => r.RowIndex == rowIndex);
+            if (rows is null || rows.Count() == 0)
+            {
+                // A cell does not exist at the specified row.
+                return null;
+            }
+            IEnumerable<Cell> cells = rows.First().Elements<Cell>().Where(c => string.Compare(c.CellReference?.Value, columnName + rowIndex, true) == 0);
+            if (cells.Count() == 0)
+            {
+                // A cell does not exist at the specified column, in the specified row.
+                return null;
+            }
+            return cells.FirstOrDefault();
+        }
+
+        private Cell CreateCell(String cellReference)
+        {
+            Cell cell = new Cell
+            {
+                CellReference = cellReference,
+                DataType = CellValues.InlineString
+            };
+
+            InlineString inlineString = new InlineString();
+            Text t = new Text
+            {
+                Text = "X"
+            };
+            inlineString.AppendChild(t);
+
+            cell.AppendChild(inlineString);
+
+            return cell;
+        }
+
+        private Cell InsertCellInWorksheet(string columnName, uint rowIndex, WorksheetPart worksheetPart)
+        {
+            Worksheet worksheet = worksheetPart.Worksheet;
+            SheetData sheetData = worksheet.GetFirstChild<SheetData>();
+            String cellReference = columnName + rowIndex;
+
+            Row row;
+            // See if the row already exists.
+            if (sheetData?.Elements<Row>().Where(r => r.RowIndex == rowIndex).Count() != 0)
+            {
+                row = sheetData.Elements<Row>().Where(r => r.RowIndex == rowIndex).First();
+            }
+            else
+            {
+                row = new Row() { RowIndex = rowIndex };
+                sheetData.Append(row);
+            }
+
+            // Find the cell.
+            Cell newCell = null;
+            if (row.Elements<Cell>().Where(c => c.CellReference.Value == columnName + rowIndex).Count() > 0)
+            {
+                newCell = row.Elements<Cell>().Where(c => c.CellReference.Value == cellReference).First();
+
+                newCell.DataType = CellValues.InlineString;
+                InlineString inlineString = new InlineString();
+                Text t = new Text
+                {
+                    Text = "X"
+                };
+                inlineString.AppendChild(t);
+                newCell.AppendChild(inlineString);
+            }
+            else
+            {
+                // Cells must be in sequential order according to CellReference.
+                // Determine where to insert the new cell.
+                Cell refCell = null;
+                foreach (Cell refCellCandidate in row.Elements<Cell>())
+                {
+                    if (string.Compare(refCellCandidate.CellReference?.Value, cellReference, true) > 0)
+                    {
+                        refCell = refCellCandidate;
+                        break;
+                    }
+                }
+
+                newCell = CreateCell(cellReference);
+                row.InsertBefore(newCell, refCell);
+            }
+
+            return newCell;
+        }
+
+        private String DetermineColumnName(String answer)
+        {
+            String columnName = "";
+            if (map.ContainsKey(answer))
+            {
+                columnName = map[answer];
+            }
+
+            return columnName;
+        }
+
+        private bool ValidateInputParams()
+        {
+            if (!File.Exists(mCSVInputfile))
+            {
+                MessageBox.Show("First set a valid CSV input file!", "Error notification");
+                return false;
+            }
+            if (!File.Exists(mXLSTemplatefile))
+            {
+                MessageBox.Show("First set a valid XLSX template file!", "Error notification");
+                return false;
+            }
+            if (!Directory.Exists(mXLSOutputFolder))
+            {
+                MessageBox.Show("First set a valid XLSX output folder!", "Error notification");
+                return false;
+            }
+            return true;
+        }
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            if(!ValidateInputParams())
+            {
+                return;
+            }
+
+            int count = 0;
+            Directory.Delete(mXLSOutputFolder, true);
+            Directory.CreateDirectory(mXLSOutputFolder);
+            using (StreamReader sr = new StreamReader(mCSVInputfile))
+            {
+                String line;
+                pbProgress.Value = 0;
+                // currentLine will be null when the StreamReader reaches the end of file
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (count > 0)
+                    {
+                        String email = "";
+                        List<String> answers = line.Split(',').ToList();
+                        if (answers.Count == map.Count + 1)
+                        {
+                            email = answers[1].Replace('.', '_').Replace('@', '_');
+                            email = email.Replace("\"", "");
+                        }
+
+                        String fileName = Path.GetFileName(mXLSTemplatefile);
+                        String outputFileName = mXLSOutputFolder + "\\" + count;
+                        if (!String.IsNullOrEmpty(email))
+                        {
+                            outputFileName += ("-" + email);
+                        }
+                        outputFileName += ("-" + fileName);
+                        File.Copy(mXLSTemplatefile, outputFileName);
+                        
+                        using (SpreadsheetDocument doc = SpreadsheetDocument.Open(outputFileName, true))
+                        {
+                            WorkbookPart workbookPart = doc.WorkbookPart;
+                            Sheet sheet = workbookPart.Workbook.Sheets.GetFirstChild<Sheet>();
+                            WorksheetPart worksheetPart = (WorksheetPart)(workbookPart.GetPartById(sheet.Id));
+
+                            uint rowNumber = 3;
+                            String columnName = "";
+                            foreach (String answer in answers)
+                            {
+                                columnName = DetermineColumnName(answer.Trim());
+                                if (!String.IsNullOrEmpty(columnName))
+                                {
+                                    Cell cell = InsertCellInWorksheet(columnName, rowNumber, worksheetPart);
+                                    rowNumber++;
+                                }
+                            }
+                        }
+                    }
+                    count++;
+                }
+            }
+
+            string message = (count - 1) + " XLS output files generated in " + mXLSOutputFolder + ".";
+            string title = "Generate XLS output";
+            MessageBox.Show(message, title);
+        }
+
+        private void btnOutputBrowse_Click(object sender, EventArgs e)
+        {
+                FolderBrowserDialog folderDlg = new FolderBrowserDialog
+                {
+                    ShowNewFolderButton = true
+                };
+                DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                mXLSOutputFolder = tbXLSOutputFolder.Text = folderDlg.SelectedPath;
+                WriteSettings();
+            }
+        }
+
+        private void ReadSettings()
+        {
+            mCSVInputfile    = ConfigurationManager.AppSettings["csv_input_file"];
+            mXLSTemplatefile = ConfigurationManager.AppSettings["xls_template_file"];
+            mXLSOutputFolder = ConfigurationManager.AppSettings["xls_output_folder"];
+        }
+        private void AddUpdateAppSettings(string key, string value)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] == null)
+                {
+                    settings.Add(key, value);
+                }
+                else
+                {
+                    settings[key].Value = value;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error writing app settings");
+            }
+        }
+        private void WriteSettings()
+        {
+            AddUpdateAppSettings("csv_input_file",    mCSVInputfile);
+            AddUpdateAppSettings("xls_template_file", mXLSTemplatefile);
+            AddUpdateAppSettings("xls_output_folder", mXLSOutputFolder);
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            WriteSettings();
+        }
+
+        private void btnXLSBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\",
+                Title = "Select XLSX template file",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "xlsx",
+                Filter = "XLSX files (*.xlsx)|*.xlsx",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                mXLSTemplatefile = tbXLSTemplate.Text = openFileDialog.FileName;
+                WriteSettings();
+            }
+        }
+    }
+}
